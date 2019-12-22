9238cd658e687aa8f1c58a37be96c1abb62d4882
Willian Teleginski <teleginski@air-de-willian.localdomain>
2019-12-22 20:28:42 -0300

    Initial commit

---
+++ Example.Test/ValuesControllerTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Example.Controllers;
+using Xunit;
+
+namespace Example.Test
+{
+    public class ValuesControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var controller = new ValuesController();
+            var response = controller.Get();
+            Assert.Equal(new string[] { "value1", "value2" }, response.Value);
+        }
+    }
+}
